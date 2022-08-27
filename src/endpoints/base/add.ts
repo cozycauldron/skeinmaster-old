a05575681f1971806412ba7be01017dfddc8de9b
Brandon <slinkardbrandon@gmail.com>
2022-08-27 09:10:36 -0500

    Initialize repository

---
+++ src/endpoints/base/add.ts
@@ -0,0 +1,16 @@
+/**
+ * Add a yarn base from a supplier
+ */
+export const add = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
